2ba7152af8b8e2656691eb7fe77161edfefac176
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-12 22:54:05 +0100

    module and controller comments

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,9 +5,12 @@ import { JwtGuard } from 'src/auth/guards/jwt-auth-guard';
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+  // Use JwtGuard to protect the route
   @UseGuards(JwtGuard)
+  // Get user profile by id
   @Get(':id')
   async getUserProfile(@Param('id') id: number) {
+    // Find user by id
     return await this.userService.findById(id);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,8 +5,16 @@ import { PrismaService } from 'src/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
+  //
   providers: [UserService, PrismaService,JwtService],
+  
   controllers: [UserController],
+  // Export UserService
+  // UserService is exported so that it can be used in other modules
+  // that import UserModule
+  // For example, AuthModule imports UserModule to use UserService
+  // to find user by email
+  // UserService is also exported so that it can be used in UserController
   exports: [UserService],
 })
 export class UserModule {}
