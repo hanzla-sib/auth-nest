16a39bee042ec228d952a069436f23dac3a35e3c
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-20 21:01:07 +0100

    destination changes

---
+++ auth-next/auth-nest/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,43 @@
+import { Backend_URL } from "@/app/lib/Constants";
+import NextAuth from "next-auth";
+import type { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "jsmith",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.username || !credentials?.password) return null;
+        const { username, password } = credentials;
+        const res = await fetch(Backend_URL + "/auth/login", {
+          method: "POST",
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (res.status === 401) {
+          console.log(res.statusText);
+          return null;
+        }
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ auth-next/auth-nest/src/app/lib/Constants.ts
@@ -0,0 +1,2 @@
+export const Backend_URL = "https://localhost:8000";
+
